9e53093f3398fe67dfef5158a02a85bba977406a
Zeuxis Lo <seekstudio@gmail.com>
2015-11-16 19:18:51 +0800

    make sure the created_at, updated_at and other date time filed don't include timezone info (tzinfo) when save to database

--- yata/yata/model.py
+++ yata/yata/model.py
@@ -5,11 +5,16 @@ from os.path import realpath, join, dirname, abspath
 from datetime import datetime
 from peewee import SqliteDatabase, Model
 from peewee import CharField, IntegerField, DateTimeField, TextField
+from util import string_to_datetime
 
 database_file = realpath(join(dirname(abspath(__file__)), '../storage/flight.db'))
 
 db = SqliteDatabase(database_file)
 db.connect()
+
+class NoTZDateTimeField(DateTimeField):
+    def db_value(self, value):
+        return value.replace(tzinfo=None)
 
 class BaseModel(Model):
     class Meta:
@@ -21,10 +26,10 @@ class Flight(BaseModel):
     ticket_price               = IntegerField(null=True)
     stay_day_min               = CharField(max_length=5, null=True)
     stay_day_max               = CharField(max_length=5, null=True)
-    valid_date_from            = DateTimeField()
-    valid_date_to              = DateTimeField()
-    valid_buy_ticket_date_from = DateTimeField()
-    valid_buy_ticket_date_to   = DateTimeField()
+    valid_date_from            = NoTZDateTimeField()
+    valid_date_to              = NoTZDateTimeField()
+    valid_buy_ticket_date_from = NoTZDateTimeField()
+    valid_buy_ticket_date_to   = NoTZDateTimeField()
     flight_info_link           = TextField(null=True)
-    created_at                 = DateTimeField(default=datetime.now)
-    update_at                  = DateTimeField(default=datetime.now)
+    created_at                 = NoTZDateTimeField(default=lambda: string_to_datetime(datetime.now().strftime('%d/%m/%Y')))
+    update_at                  = NoTZDateTimeField(default=lambda: string_to_datetime(datetime.now().strftime('%d/%m/%Y')))
